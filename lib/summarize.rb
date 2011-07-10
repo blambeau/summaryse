b0d03226d4108ae866b298616923405e332f943c
Bernard Lambeau <blambeau@gmail.com>
2011-07-10 16:57:54 +0200

    Imported first features from sandbox

--- lib/summarize.rb
+++ lib/summarize.rb
@@ -1,8 +1,31 @@
-#
-# Array#summarize
-#
-module Summarize
-  
-end # module Summarize
+class Array
+
+  def summarize(agg)
+    case agg
+    when NilClass
+      first
+    when :avg
+      inject(:+).to_f/size;
+    when :intersection
+      inject(:&)
+    when :sum
+      inject(:+)
+    when :union
+      inject(:|)
+    when Symbol
+      self.send(agg)
+    when Hash
+      big = Hash.new{|h,k| h[k] = []}
+      each{|tuple| tuple.each_pair{|k,v| big[k] << v}}
+      Hash[big.collect{|k,v| [k,v.summarize(agg[k])]}]
+    when Array
+      by, agg = agg
+      grouped = Hash.new{|h,k| h[k] = []}
+      flatten.each{|tuple| grouped[tuple[by]] << tuple}
+      grouped.values.collect{|rel| rel.summarize(agg)}
+    end
+  end
+
+end
 require "summarize/version"
 require "summarize/loader"
--- spec/summarize_spec.rb
+++ spec/summarize_spec.rb
@@ -4,5 +4,34 @@ describe Summarize do
   it "should have a version number" do
     Summarize.const_defined?(:VERSION).should be_true
   end
+
+  let(:numbers){ [15, 3, 17, 4, 12] } 
+  let(:arrays){ [[15, 3, 12], [17, 4, 12]] } 
+
+  describe "when called with a simple symbol" do
+    
+    it "should recognize avg" do
+      numbers.summarize(:avg).should eq((15 + 3 + 17 + 4 + 12)/5.0)
+    end
+
+    it "should recognize intersection" do
+      arrays.summarize(:intersection).should eq([12])
+    end
+
+    it "should recognize sum" do
+      numbers.summarize(:sum).should eq(15 + 3 + 17 + 4 + 12)
+      arrays.summarize(:sum).should eq([ 15, 3, 12, 17, 4, 12])
+    end
+
+    it "should recognize union" do
+      arrays.summarize(:union).should == [15, 3, 12, 17, 4]
+    end
+
+    it "should send it to the numbers by default" do
+      numbers.summarize(:min).should == 3
+      numbers.summarize(:max).should == 17
+    end
+
+  end
   
 end
