78149f4137a4588a8bf32b49a902c07ace60a693
Bernard Lambeau <blambeau@gmail.com>
2011-07-11 13:33:04 +0200

    Added YARD doc to Array#summaryse

--- lib/summaryse/core_ext/array.rb
+++ lib/summaryse/core_ext/array.rb
@@ -1,5 +1,63 @@
 class Array
 
+  #
+  # Apply a summarization to self
+  #
+  # == Base
+  #
+  # Basic summarization operators are 
+  #   * :avg, :count, :sum
+  #   * :mix, :max, :first, :last
+  #   * :union, :intersection
+  #
+  # They suppose the existence of the corresponding dyadic operators like 
+  # '+' (i.e. sum, avg), '|' (union), '&' (intersection) on the array values.
+  #
+  # Example:
+  #     [1, 4, 12, 7].summaryse(:sum)   # => 24
+  #
+  # == Hashes
+  #
+  # Summarizing arrays of hashes may be done by passing an hash of summarization
+  # expressions. Calls will be made recursively in this case:
+  #
+  # Example: 
+  #     [
+  #       { :hobbies => [:ruby],  :size => 12 },
+  #       { :hobbies => [:music], :size => 17 }
+  #     ].summaryse(:hobbies => :union, :size => :max)
+  #     # => {:hobbies => [:ruby, :music], :size => 17}
+  #
+  # Use the nil key to specify the default aggregation to apply if not explcitely
+  # specified. A Proc object can also be passed as aggregation operator. In this
+  # case, it will be called with the array of values on which the aggregation
+  # must be done:
+  #
+  #     # In the example below, hobbies are summarized through default behavior
+  #     # provided by the nil key. Sizes are summarized by the lambda. 
+  #     [
+  #       { :hobbies => [:ruby],  :size => 12 },
+  #       { :hobbies => [:music], :size => 17 }
+  #     ].summaryse(nil => :union, :size => lambda{|a| a.join(',')})
+  #     # => {:hobbies => [:ruby, :music], :size => "12,17"}
+  # 
+  # == Arrays of Hashes
+  #
+  # Summarizing arrays of arrays of hashes may be done by passing an array of 
+  # two values. The first one is a 'by key', the second is the summarization 
+  # hash to apply.
+  #
+  # Example:
+  #     [ 
+  #       [ { :name => :yard,      :for => [ :devel   ] },
+  #         { :name => :summaryse, :for => [ :runtime ] } ],
+  #       [ { :name => :summaryse, :for => [ :devel   ] }, 
+  #         { :name => :treetop,   :for => [ :runtime ] } ]
+  #     ].summaryse([ [:name], {:for => :union} ])
+  #     # => [ {:name => :yard,      :for => [:devel]           },
+  #     #      {:name => :summaryse, :for => [:devel, :runtime] },
+  #     #      {:name => :treetop,   :for => [:runtime]         } ]
+  #
   def summaryse(agg)
     case agg
     when Proc
